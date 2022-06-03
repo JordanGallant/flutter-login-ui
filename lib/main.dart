4e8f666078f70fc7f24d52b98562f34c436e3ed8
JordanGallant <39-12602@sae.edu.za>
2022-06-03 10:52:06 +0200

    init

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,15 @@ void main() {
 ColorScheme defaultColorScheme = const ColorScheme(
   primary: Color(0xffBB86FC),
   secondary: Color(0xff03DAC6),
-  surface: Color(0xff181818),
+  surface: Color.fromARGB(255, 0, 208, 255),
   background: Color(0xff121212),
   error: Color(0xffCF6679),
-  onPrimary: Color(0xff000000),
-  onSecondary: Color(0xff000000),
+  onPrimary: Color(0xff000000), // button text
+  onSecondary: Color.fromARGB(255, 173, 39, 39),
   onSurface: Color(0xffffffff),
   onBackground: Color(0xffffffff),
   onError: Color(0xff000000),
-  brightness: Brightness.dark,
+  brightness: Brightness.dark, // grey text
 );
 
 class MyApp extends StatelessWidget {
